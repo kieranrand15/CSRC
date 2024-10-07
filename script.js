2b6382aefd765fac392515e1c40399ae3aeedc7a
kieranrand15 <randdatasecsolutions@gmail.com>
2024-10-07 20:33:36 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarlinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarlinks.classList.toggle('active')
-})
 
